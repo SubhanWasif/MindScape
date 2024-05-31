6980591b65e8446437458cb85427fc6798c27b44
SubhanWasif <subhanwasif21@gmail.com>
2024-05-31 22:58:58 +0500

    Added blurryhash for the images

--- src/components/MeetTeam.js
+++ src/components/MeetTeam.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Back from "../assets/back.png";
 import { useNavigate } from "react-router-dom";
 import subhan from "../assets/subhaaan 1.png";
-import hamza from "../assets/Frame 3.png";
+import hamza from "../assets/hamza3.png";
 import maheen from "../assets/maheeen 1.png";
+import { Blurhash } from "react-blurhash";
 
 export default function MeetTheTeam() {
+  const [loading, setLoading] = useState({
+    subhan: false,
+    hamza: false,
+    maheen: false,
+  });
+
+  useEffect(() => {
+    const loadImage = (imageSrc, key) => {
+      const img = new Image();
+      img.onload = () => {
+        setLoading((prev) => ({ ...prev, [key]: true }));
+      };
+      img.src = imageSrc;
+    };
+
+    loadImage(subhan, "subhan");
+    loadImage(hamza, "hamza");
+    loadImage(maheen, "maheen");
+  }, []);
+
   let navigate = useNavigate();
   return (
     <div
-      className="gradient "
+      className="gradient"
       style={{
         margin: "0",
         height: "100vh",
@@ -40,20 +61,8 @@ export default function MeetTheTeam() {
           color: "white",
         }}
       >
-        <div
-          style={{
-            fontSize: "4rem",
-          }}
-        >
-          MindSCAPE
-        </div>
-        <div
-          style={{
-            fontSize: "2rem",
-          }}
-        >
-          VISUALIZING THOUGHTS
-        </div>
+        <div style={{ fontSize: "4rem" }}>MindSCAPE</div>
+        <div style={{ fontSize: "2rem" }}>VISUALIZING THOUGHTS</div>
       </div>
       <div
         className="ml-[100px]"
@@ -71,20 +80,46 @@ export default function MeetTheTeam() {
         }}
       >
         <div className=" flex justify-center items-center gap-[40px] pt-[40px]">
-          <div className="flex justify-center flex-col items-center gap-[30px]">
-            <img className="rounded-3xl" src={subhan} alt="subhan" />
-            <div className="text-2xl">Subhan Wasif</div>
-          </div>
-          <div className="flex justify-center flex-col items-center gap-[30px]">
-            <img className="rounded-3xl" src={hamza} alt="subhan" />
-            <div className="text-2xl">Hamza Ahmad</div>
-          </div>
-          <div className="flex justify-center flex-col items-center gap-[30px]">
-            <img className="rounded-3xl" src={maheen} alt="subhan" />
-            <div className="text-2xl">Maheen Khurram</div>
-          </div>
+          <Person
+            loading={loading.subhan}
+            imgSrc={subhan}
+            blurHash="Lf9*ozj[o#j[.AkCayoetRfQV@j?"
+            name="Subhan Wasif"
+          />
+          <Person
+            loading={loading.hamza}
+            imgSrc={hamza}
+            blurHash="LGDm5X~p.8D%DiMxM_ofoJxvRjRj"
+            name="Hamza Ahmad"
+          />
+          <Person
+            loading={loading.maheen}
+            imgSrc={maheen}
+            blurHash="LCC#xb1M^kkW}q9tRjM{X9-VxaSh"
+            name="Maheen Khurram"
+          />
         </div>
       </div>
+    </div>
+  );
+}
+
+function Person({ loading, imgSrc, blurHash, name }) {
+  return (
+    <div className="flex justify-center flex-col items-center gap-[30px]">
+      {!loading ? (
+        <Blurhash
+          hash={blurHash}
+          width={369}
+          height={339}
+          resolutionX={32}
+          resolutionY={32}
+          punch={1}
+        />
+      ) : (
+        <img className="rounded-3xl" src={imgSrc} alt={name} />
+      )}
+      <div className="text-2xl">{name}</div>
     </div>
   );
 }
